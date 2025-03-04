93820e87da33afc908c374ba45f495db55478d5f
Robson Pinheiro <robson.rogerio@gmail.com>
2025-03-04 16:06:13 -0300

    noçoes de layout

---
+++ app.py
@@ -0,0 +1,46 @@
+import dash_bootstrap_components as dbc
+from dash import html
+import dash
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
+
+card_content = [
+    dbc.CardHeader("Card header"),
+    dbc.CardBody(
+        [
+            html.H5("Card title", className="card-title"),
+            html.P(
+                "This is some card content that we'll reuse",
+                className="card-text",
+            ),
+        ]
+    ),
+]
+
+app.layout = html.Div([
+    dbc.Row([
+        dbc.Col([
+            dbc.Card(
+                card_content, color='primary', inverse=True, 
+                style={'height': '90vh', 'margin': '10px', 'padding-left': '10px'})
+        ], sm=4),
+
+        dbc.Col([
+            dbc.Row([
+                dbc.Col(dbc.Card(card_content, color='info', inverse=True)),
+                dbc.Col(dbc.Card(card_content, color='info', inverse=True))
+            ]),
+            dbc.Row([
+                dbc.Col(dbc.Card(card_content, color='error'), md=4),
+                dbc.Col(dbc.Card(card_content, color='error'), md=4),
+                dbc.Col(dbc.Card(card_content, color='error'), md=4)
+            ])
+        ])
+    ])
+])
+
+
+# ============== Run Server ============== #
+if __name__ == '__main__':
+    app.run_server(port=8050, debug=True)
--- index.py
+++ index.py
@@ -7,8 +7,12 @@ import numpy as np
 
 import plotly.express as px
 import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__)
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.DARKLY]
+)
+
 server = app.server
 
 df_data = pd.read_csv('supermarket_sales.csv')
@@ -17,13 +21,13 @@ df_data['Date'] = pd.to_datetime(df_data['Date'])
 # ============== Layout ============== #
 app.layout = html.Div(children=[
                 html.H5("Cidades:"),
-                dcc.Checklist(
-                    df_data['City'].value_counts().index,
-                    df_data['City'].value_counts().index,
-                    id='check_city'),
+                            dcc.Checklist(df_data["City"].value_counts().index,
+                            df_data["City"].value_counts().index, id="check_city",
+                            inputStyle={"margin-right": "5px", "margin-left": "20px"}),
 
-                html.H5('Variável de análise:'),
-                dcc.RadioItems(['gross income', 'Rating'], 'gross income', id='main_variable'),
+                            html.H5("Variável de análise:", style={"margin-top": "30px"}),
+                            dcc.RadioItems(["gross income", "Rating"], "gross income", id="main_variable",
+                            inputStyle={"margin-right": "5px", "margin-left": "20px"}),
 
                 dcc.Graph(id='city_fig'),
                 dcc.Graph(id='pay_fig'),
@@ -57,9 +61,9 @@ def render_graphs(cities, main_variable):
     fig_payment = px.bar(df_payment, y='Payment', x=main_variable, orientation='h')
     fig_product_income = px.bar(df_product_income, x=main_variable, y='Product line', color='City', orientation='h', barmode='group')
     
-    fig_city.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200)
-    fig_payment.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200)
-    fig_product_income.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=500)
+    fig_city.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200, template='plotly_dark')
+    fig_payment.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200, template='plotly_dark')
+    fig_product_income.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=500, template='plotly_dark')
 
     return fig_city, fig_payment, fig_product_income
 

1fb70fe342c69d0b3401b0e84715ed141f709043
Robson Pinheiro <robson.rogerio@gmail.com>
2025-03-04 17:20:32 -0300

    inserção novos graficos

--- index.py
+++ index.py
@@ -9,10 +9,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(
-    external_stylesheets=[dbc.themes.MINTY]
-)
-
+app = dash.Dash(external_stylesheets=[dbc.themes.MINTY])
 server = app.server
 
 df_data = pd.read_csv('supermarket_sales.csv')
@@ -36,9 +33,13 @@ app.layout = html.Div(children=[
                             ], style={'height': '90vh', 'margin': '20px', 'padding': '15px'})
                     ], sm=2),                    
                     dbc.Col([
-                        dcc.Graph(id='city_fig'),
-                        dcc.Graph(id='pay_fig'),
-                        dcc.Graph(id='income_per_product_fig')
+                        dbc.Row([
+                            dbc.Col([dcc.Graph(id='city_fig')], sm=4),
+                            dbc.Col([dcc.Graph(id='gender_fig')], sm=4),
+                            dbc.Col([dcc.Graph(id='pay_fig')], sm=4)
+                        ]),
+                        dbc.Row([dcc.Graph(id='income_per_date_fig')]),
+                        dbc.Row([dcc.Graph(id='income_per_product_fig')])
                     ], sm=10)
                 ])
             ]
@@ -49,7 +50,9 @@ app.layout = html.Div(children=[
     [
         Output('city_fig', 'figure'),
         Output('pay_fig', 'figure'),
-        Output('income_per_product_fig', 'figure'),
+        Output('gender_fig', 'figure'),
+        Output('income_per_date_fig', 'figure'),
+        Output('income_per_product_fig', 'figure')        
     ],
     [
         Input('check_city', 'value'),
@@ -63,19 +66,23 @@ def render_graphs(cities, main_variable):
     operation = np.sum if main_variable == 'gross income' else np.mean
     df_filtered = df_data[df_data['City'].isin(cities)]
     df_city = df_filtered.groupby('City')[main_variable].apply(operation).to_frame().reset_index()
+    df_gender = df_filtered.groupby(['Gender', 'City'])[main_variable].apply(operation).to_frame().reset_index()
     df_payment = df_filtered.groupby('Payment')[main_variable].apply(operation).to_frame().reset_index()
+    df_income_time = df_filtered.groupby('Date')[main_variable].apply(operation).to_frame().reset_index()
     df_product_income = df_filtered.groupby(['Product line','City'])[main_variable].apply(operation).to_frame().reset_index()
 
     fig_city = px.bar(df_city, x='City', y=main_variable)
     fig_payment = px.bar(df_payment, y='Payment', x=main_variable, orientation='h')
+    fig_gender = px.bar(df_gender, y=main_variable, x='Gender', color='City', barmode='group')
     fig_product_income = px.bar(df_product_income, x=main_variable, y='Product line', color='City', orientation='h', barmode='group')
+    fig_income_date = px.bar(df_income_time, y=main_variable, x='Date')
     
     fig_city.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200)
     fig_payment.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=200)
     fig_product_income.update_layout(margin=dict(l=0, r=0, t=20, b=20), height=500)
 
-    return fig_city, fig_payment, fig_product_income
+    return fig_city, fig_payment, fig_gender, fig_income_date, fig_product_income
 
-# ============== Run Srrver ============== #
+# ============== Run Server ============== #
 if __name__ == '__main__':
     app.run_server(port=8050, debug=True)
